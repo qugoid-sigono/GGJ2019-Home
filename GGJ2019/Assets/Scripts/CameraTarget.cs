0d435a3aebe3c7bc2ab5a436757237339494f662
kylejwo <uxjxukylejwo@gmail.com>
2019-01-26 17:57:42 +0800

    pick fire wood and camera target

---
+++ GGJ2019/Assets/Scripts/CameraTarget.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTarget : MonoBehaviour
+{
+	public GameObject targetObj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if(targetObj){
+			this.GetComponent<Transform>().position = new Vector3(targetObj.GetComponent<Transform>().position.x, targetObj.GetComponent<Transform>().position.y, this.GetComponent<Transform>().position.z);
+		}
+    }
+}
---
+++ GGJ2019/Assets/Scripts/ItemTrigger.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemTrigger : MonoBehaviour
+{
+	private GameManager gameManager = new GameManager();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		if (gameManager == null)
+		{
+			gameManager = GameManager.instance;
+		}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if(gameManager){
+			gameManager.currentItemHold++;
+			Debug.Log("Item Picked Up! Now ItemNum = "+gameManager.currentItemHold);
+			Destroy(this.gameObject);
+		}else{
+			Debug.Log("There is no gameManager on this scece!");
+		}
+	}
+}
